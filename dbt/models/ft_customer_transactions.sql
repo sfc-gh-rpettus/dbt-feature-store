5622f79a71643505531370f18467d51f553f4173
Randy Pettus <randy.pettus@snowflake.com>
2024-08-23 18:17:42 +0000

    changed transaction times back

--- dbt/models/ft_customer_transactions.sql
+++ dbt/models/ft_customer_transactions.sql
@@ -6,9 +6,9 @@ select
     {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '1 days', 'sum') }} as TX_AMOUNT_1D,
     {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '7 days', 'sum') }} as TX_AMOUNT_7D,
     {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '30 days', 'sum') }} as TX_AMOUNT_30D,
-    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '1 days', 'avg') }} as AVG_TX_AMOUNT_10D,
-    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '7 days', 'avg') }} as AVG_TX_AMOUNT_7D,
-    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '30 days', 'avg') }} as AVG_TX_AMOUNT_30D,
+    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '1 days', 'avg') }} as TX_AMOUNT_AVG_1D,
+    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '7 days', 'avg') }} as TX_AMOUNT_AVG_7D,
+    {{ rolling_agg('TX_AMOUNT', 'CUSTOMER_ID', 'TX_DATETIME', '30 days', 'avg') }} as TX_AMOUNT_AVG_30D,
     {{ rolling_agg('*', 'CUSTOMER_ID', 'TX_DATETIME', '1 days', 'count') }} as TX_CNT_1D,
     {{ rolling_agg('*', 'CUSTOMER_ID', 'TX_DATETIME', '7 days', 'count') }} as TX_CNT_7D,
     {{ rolling_agg('*', 'CUSTOMER_ID', 'TX_DATETIME', '30 days', 'count') }} as TX_CNT_30D
